a8227fb773116ca0de063bdaffd7adeb82383d99
Dalton Rogers <dcwolfpak@gmail.com>
2025-02-12 10:05:13 -0500

    Added a way to check the os.

--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -1,3 +1,24 @@
+use std::process::Command;
+/*
+This program is responsible for setting up the polyglot system.
+
+@author Dalton Rogers, Preston Knepper
+@version
+*/
+
 fn main() {
-    println!("Hello, world!");
+    // checks what os is being used and calls the appropriate setup
+    if cfg!(target_os = "windows") {
+        windows_setup();
+    } else {
+        linux_setup();
+    }
+}
+
+fn windows_setup(){
+    let status = Command::new("runas")
+}
+
+fn linux_setup(){
+    let status = Command::new("sudo");
 }
