4170e918e64566b988d38c3a0786abd9b8fcd0eb
Dalton Rogers <dcwolfpak@gmail.com>
2025-02-12 19:49:36 -0500

    Commented the lib file

--- rust_backend/src/lib.rs
+++ rust_backend/src/lib.rs
@@ -1,10 +1,19 @@
 use std::io::Result;
-
 mod unix;
 mod windows;
 pub use crate::unix::Unix;
 pub use crate::windows::Windows;
+/// 
+/// This module is responsible for performing either the setup or the teardown depending 
+/// on the user input.
+/// Author Preston Knepper
+/// Version 2/12/25
+/// 
 
+///
+/// This function will determine the target operating system that the setup and
+/// teardown will be performed on. Either Windows or Unix. 
+/// 
 pub fn start(action: Action) {
     // checks what os is being used and calls the appropriate setup
     if cfg!(target_os = "windows") {
@@ -12,7 +21,7 @@ pub fn start(action: Action) {
             Action::Setup(perform_on) => Windows::setup(perform_on),
             Action::Teardown(perform_on) => Windows::teardown(perform_on),
         }
-    } else {
+    } else { // this does the unix setup
         match action {
             Action::Setup(perform_on) => Unix::setup(perform_on),
             Action::Teardown(perform_on) => Unix::teardown(perform_on),
@@ -20,11 +29,20 @@ pub fn start(action: Action) {
     }
 }
 
+///
+/// The Action enum is responsible for classifying what we are performing, in this
+/// case either Setup or Teardown. PerformOn indicates which systems the Action will
+/// be performed on.
+/// 
 pub enum Action {
     Setup(PerformOn),
     Teardown(PerformOn),
 }
 
+///
+/// The PerformOn enum is responsible for classifying the database system we will be
+/// performing the Action on. Can either be all of them, or one of them at a time.
+/// 
 pub enum PerformOn {
     All,
     Postgres,
@@ -32,6 +50,11 @@ pub enum PerformOn {
     Neo4j,
 }
 
+///
+/// Acts like an Interface for Setup, highlighting what needs to be implemented
+/// by Setup. This includes methods for setting up all the containers, or just one
+/// specific container.
+/// 
 pub trait Setup {
     fn setup(perform_on: PerformOn);
     fn setup_postgres() -> Result<()>;
@@ -39,6 +62,11 @@ pub trait Setup {
     fn setup_neo4j() -> Result<()>;
 }
 
+/// 
+/// Acts like an Interface for Teardown, highlighting what needs to be 
+/// implemented by Teardown. This includes methods for tearing down all
+/// of the containers, or just one specific container.
+/// 
 pub trait Teardown {
     fn teardown(perform_on: PerformOn);
     fn teardown_postgres() -> Result<()>;
