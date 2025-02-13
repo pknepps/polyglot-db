21c54b12630a6992414977252d063571edfd8f34
pknepps <pknepps@outlook.com>
2025-02-12 19:28:57 -0500

    setup/teardown of containers done, schemas and configs must still be set

--- rust_backend/src/unix.rs
+++ rust_backend/src/unix.rs
@@ -7,9 +7,9 @@ impl Setup for Unix {
     fn setup(perform_on: PerformOn) {
         if let Err(error) = match perform_on{
             PerformOn::All => {
-                Unix::setup_mongodb();
-                Unix::setup_neo4j();
-                Unix::setup_postgres();
+                Unix::setup(PerformOn::MongoDB);
+                Unix::setup(PerformOn::Neo4j);
+                Unix::setup(PerformOn::Postgres);
                 Ok(())
             },
             PerformOn::MongoDB => {
@@ -27,25 +27,43 @@ impl Setup for Unix {
     }
 
     fn setup_mongodb() -> std::io::Result<()> {
-        todo!()
+        println!("Creating MongoDB container...");
+        run_command("docker pull mongodb/mongodb-community-server:latest")?;
+        run_command(
+            "docker run \
+                --name polyglot-mongodb \
+                -p 27017:27017 \
+                -d \
+                mongodb/mongodb-community-server:latest"
+        )?;
+        Ok(())
     }
 
     fn setup_neo4j() -> std::io::Result<()> {
-        let output = Command::new("sh")
-            .arg("-c")
-            .arg(
-                "docker run \
+        let password = include_str!("../../backend/app/NEO4J_PASSWORD");
+        println!("Creating Neo4j container...");
+        run_command(&format!(
+            "docker run \
+                --name=polyglot-neo4j \
                 --publish=7474:7474 \
                 --publish=7687:7687 \
-                --env NEO4J_AUTH=neo4j/jojygjojs \
-                neo4j:5.24.1 \
-                --detach"
-            ).output()?;
+                --env NEO4J_AUTH=neo4j/{password} \
+                -d \
+                neo4j:5.24.1"
+        ))?;
         Ok(())
     }
 
     fn setup_postgres() -> std::io::Result<()> {
-        todo!();
+        let password = include_str!("../../backend/app/POSTGRES_PASSWORD");
+        println!("Creating PostgreSQL container...");
+        run_command(&format!(
+            "docker run \
+                --name=polyglot-postgres \
+                -e POSTGRES_PASSWORD={password} \
+                -d \
+                postgres"
+        ))?;
         Ok(())
     }
 }
@@ -54,9 +72,9 @@ impl Teardown for Unix {
     fn teardown(perform_on: PerformOn) {
         if let Err(error) = match perform_on{
             PerformOn::All => {
-                Unix::teardown_mongodb();
-                Unix::teardown_neo4j();
-                Unix::teardown_postgres();
+                Unix::teardown(PerformOn::MongoDB);
+                Unix::teardown(PerformOn::Neo4j);
+                Unix::teardown(PerformOn::Postgres);
                 Ok(())
             },
             PerformOn::MongoDB => {
@@ -74,17 +92,33 @@ impl Teardown for Unix {
     }
 
     fn teardown_mongodb() -> std::io::Result<()> {
-        todo!();
+        println!("Tearing down MongoDB container...");
+        run_command("docker stop polyglot-mongodb")?;
+        run_command("docker rm polyglot-mongodb")?;
         Ok(())
     }
 
     fn teardown_neo4j() -> std::io::Result<()> {
-        todo!();
+        println!("Tearing down Neo4j container...");
+        run_command("docker stop polyglot-neo4j")?;
+        run_command("docker rm polyglot-neo4j")?;
         Ok(())
     }
 
     fn teardown_postgres() -> std::io::Result<()> {
-        todo!();
+        println!("Tearing down PostgreSQL container...");
+        run_command("docker stop polyglot-postgres")?;
+        run_command("docker rm polyglot-postgres")?;
         Ok(())
     }
+}
+
+fn run_command(command: &str) -> std::io::Result<()> {
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(command)
+        .output()?;
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+    eprintln!("{}", String::from_utf8_lossy(&output.stderr));
+    Ok(())
 }
