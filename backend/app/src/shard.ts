97b487d4e59d52d2b7499f6d8ec2bb5c58f59242
pknepps <pknepps@outlook.com>
2025-02-26 19:47:44 -0500

    started on sharding algorithm

---
+++ backend/app/src/shard.ts
@@ -0,0 +1,9 @@
+export async function getNeo4JAdress(): Promise<String> {
+    return "hi"
+}
+export async function getPostgresAddress(): Promise<String> {
+    return "hi"
+}
+export async function getMongoAdress(): Promise<String> {
+    return "hi"
+}
