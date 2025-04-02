de8fb8b3e5a63075f9ba1eebb57de83946d1baff
kevinmccall <erakiproj99@gmail.com>
2025-04-02 16:27:27 -0400

    replaced postgres with yugabyte

--- rust_backend/src/lib.rs
+++ rust_backend/src/lib.rs
@@ -7,22 +7,22 @@ pub use crate::teardown::teardown;
 /// #Lib
 /// This module is where we define our Enums used throughout the program, such
 /// as Action, PerformOn, and OS. We also have a start method which checks what
-/// the user's OS is and run_command is responsible for running the various 
+/// the user's OS is and run_command is responsible for running the various
 /// Docker commands.
-/// 
+///
 /// **Author**: Preston Knepper
 /// **Version**: 2/12/25
 
-/// The Action enum is responsible for classifying what we are performing, in 
-/// this case either Setup or Teardown. PerformOn indicates which systems the 
+/// The Action enum is responsible for classifying what we are performing, in
+/// this case either Setup or Teardown. PerformOn indicates which systems the
 /// Action will be performed on.
 pub enum Action {
     Setup(PerformOn),
     Teardown(PerformOn),
 }
 
-/// The PerformOn enum is responsible for classifying the database system we 
-/// will be performing the Action on. Can either be all of them, or one of 
+/// The PerformOn enum is responsible for classifying the database system we
+/// will be performing the Action on. Can either be all of them, or one of
 /// them at a time.
 pub enum PerformOn {
     All,
@@ -41,8 +41,8 @@ pub enum OS {
 }
 
 /// This will determine the OS of the user and call the proper setup or
-/// teardown. 
-/// 
+/// teardown.
+///
 /// **Param** action: Either Setup or Teardown.
 pub fn start(action: Action) {
     // checks what os is being used and calls the appropriate setup
@@ -51,7 +51,8 @@ pub fn start(action: Action) {
             Action::Setup(perform_on) => setup(perform_on, OS::Windows),
             Action::Teardown(perform_on) => teardown(perform_on, OS::Windows),
         }
-    } else { // setup and teardown for unix
+    } else {
+        // setup and teardown for unix
         match action {
             Action::Setup(perform_on) => setup(perform_on, OS::Unix),
             Action::Teardown(perform_on) => teardown(perform_on, OS::Unix),
@@ -60,22 +61,16 @@ pub fn start(action: Action) {
 }
 
 /// Takes a string and a operating system, uses this information to run the
-/// command line for the correct operating system. 
-/// 
+/// command line for the correct operating system.
+///
 /// **Param** command: The command for the command line.
 /// **Param** os: The OS variant.
-/// **Return**: Will return nothing if all goes well, otherwise throws the 
+/// **Return**: Will return nothing if all goes well, otherwise throws the
 /// output.
 fn run_command(command: &str, os: OS) -> std::io::Result<()> {
     let _child = match os {
-        OS::Windows => Command::new("cmd")
-            .arg("/C")
-            .arg(command)
-            .spawn()?,
-        OS::Unix => Command::new("sh")
-            .arg("-c")
-            .arg(command)
-            .spawn()?,
+        OS::Windows => Command::new("cmd").arg("/C").arg(command).status()?,
+        OS::Unix => Command::new("sh").arg("-c").arg(command).status()?,
     };
     Ok(())
 }
