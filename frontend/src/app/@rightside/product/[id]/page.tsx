15bf8a0f863f084033362e9f4e0bc06c98f3af21
kevinmccall <erakiproj99@gmail.com>
2025-04-04 11:07:10 -0400

    fixed right side and fixed home button not working

--- frontend/src/app/@rightside/product/[id]/page.tsx
+++ frontend/src/app/@rightside/product/[id]/page.tsx
@@ -1,25 +1,23 @@
-import { getPostgresData, getNeoGraph, getProduct } from '@/app/request';
-import { RightSide } from '@/app/right_side_component';
+import { getPostgresData, getNeoGraph, getProduct, getRedisData } from "@/app/request";
+import { RightSide } from "@/app/right_side_component";
 
-export default async function RightSideSingleProduct({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const productId = Number((await params).id);
-  if (isNaN(productId)) {
-    console.log(productId);
-    return 'Invalid id passed in.';
-  }
-  const postgresData = await getPostgresData(productId);
-  const neoGraphData = await getNeoGraph(productId);
-  const mongoSchema = await getProduct(productId);
+export default async function RightSideSingleProduct({ params }: { params: Promise<{ id: string }> }) {
+    const productId = Number((await params).id);
+    if (isNaN(productId)) {
+        console.log(productId);
+        return "Invalid id passed in.";
+    }
+    const postgresData = await getPostgresData(productId);
+    const neoGraphData = await getNeoGraph(productId);
+    const mongoSchema = await getProduct(productId);
+    const redisData = await getRedisData(productId);
 
-  return (
-    <RightSide
-      postgresData={postgresData}
-      mongoSchema={mongoSchema}
-      neoGraphData={neoGraphData}
-    ></RightSide>
-  );
+    return (
+        <RightSide
+            postgresData={postgresData}
+            mongoSchema={mongoSchema}
+            neoGraphData={neoGraphData}
+            redisData={redisData}
+        ></RightSide>
+    );
 }
--- frontend/src/app/right_side_component.tsx
+++ frontend/src/app/right_side_component.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 /**
  * This module is responsible for creating the right side component.
  *
@@ -6,19 +6,11 @@
  * @Version 12/4/2024
  */
 
-import React, { useEffect, useRef, ReactNode, useState } from 'react';
-import './right_side_component.css';
-import { MongoSchema, NeoGraph, PostgresDataTable, RedisDataTable } from './components';
-import { 
-    getPostgresData, 
-    getNeoGraph, 
-    getMongoSchema, 
-    getRedisData,
-    PostgresQueryData, 
-    Neo4jQueryData,
-    RedisQueryData,
-} from './request';
-import { setRequestMeta } from 'next/dist/server/request-meta';
+import React, { useState } from "react";
+import "./right_side_component.css";
+import { MongoSchema, NeoGraph, PostgresDataTable, RedisDataTable } from "./components";
+import { JsonData } from "json-edit-react";
+import { Neo4jQueryData, PostgresQueryData, RedisQueryData } from "./request";
 
 export enum DbName {
     postgres,
@@ -32,46 +24,38 @@ export enum DbName {
  *
  * @returns The right side component.
  */
-export function RightSide() {
+export function RightSide({
+    postgresData,
+    mongoSchema,
+    neoGraphData,
+    redisData,
+}: {
+    postgresData: PostgresQueryData[];
+    mongoSchema: JsonData;
+    neoGraphData: Neo4jQueryData;
+    redisData: RedisQueryData;
+}) {
     const [selected, setSelected] = useState<DbName>(DbName.postgres);
-    const [postgresData, setPostgresData] = useState<PostgresQueryData[]>([]);
-    const [neoGraphData, setNeoGraphData] = useState<Neo4jQueryData>({nodes: [], edges: []});
-    const [mongoSchema, setMongoSchema] = useState<any>();
-    const [redisData, setRedisData] = useState<RedisQueryData | null>(null);
 
-    useEffect( () => {
-        async function getData() {
-            setPostgresData(await getPostgresData());
-            setMongoSchema(await getMongoSchema());
-            setNeoGraphData(await getNeoGraph());
-            setRedisData(await getRedisData(2))
-        }
-        getData();
-    }, [])
     // the html for the right side
     return (
-        <div className='h-dvh bg-sky-100 w-full py-4 px-8'>
-            <div className='buttons-container'>
-                <button
-                    className='btn py-2'
-                    onClick={() => setSelected(DbName.postgres)}
-                >
+        <div className="h-dvh bg-sky-100 w-full py-4 px-8">
+            <div className="buttons-container">
+                <button className="btn py-2" onClick={() => setSelected(DbName.postgres)}>
                     PostgreSQL
                 </button>
-                <button className='btn py-2' onClick={() => setSelected(DbName.mongo)}>
+                <button className="btn py-2" onClick={() => setSelected(DbName.mongo)}>
                     MongoDB
                 </button>
-                <button className='btn py-2' onClick={() => setSelected(DbName.neo4j)}>
+                <button className="btn py-2" onClick={() => setSelected(DbName.neo4j)}>
                     Neo4j
                 </button>
-                <button className='btn py-2' onClick={() => setSelected(DbName.redis)}>
+                <button className="btn py-2" onClick={() => setSelected(DbName.redis)}>
                     Redis
                 </button>
             </div>
-            <div className='h-full'>
-                {selected === DbName.postgres && (
-                    <PostgresDataTable data={postgresData} />
-                )}
+            <div className="h-full">
+                {selected === DbName.postgres && <PostgresDataTable data={postgresData} />}
                 {selected === DbName.mongo && <MongoSchema schema={mongoSchema} />}
                 {selected === DbName.neo4j && <NeoGraph data={neoGraphData} />}
                 {selected === DbName.redis && <RedisDataTable data={redisData} />}
