2a33a4171c645d11c2e3c63dbe1eafc524b0ac9f
kevinmccall <kevinmccalldev@gmail.com>
2025-04-03 21:11:05 -0400

    made global right side component default whenever doing hard refresh

--- frontend/src/app/@rightside/furthertesting.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { use } from "react";
-
-export default function PromiseEater({ promise }: { promise: Promise<any> }) {
-    const data = use(promise);
-    return <div>{JSON.stringify(data)}</div>;
-}
